87f94a3f2dcc86991310aab6799991f3760c5cf8
Korofe <federica.manograssi@gmail.com>
2024-02-05 09:11:21 +0100

    Initial
    creazione repository

---
+++ src/main/java/example/cinema/CinemaBisApplication.java
@@ -0,0 +1,13 @@
+package example.cinema;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CinemaBisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CinemaBisApplication.class, args);
+	}
+
+}
